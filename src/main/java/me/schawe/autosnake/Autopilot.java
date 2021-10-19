2c23121f2b8285f4bf649288816bd99446177e0e
Hendrik Schawe <hendrik.schawe@codecentric.de>
2021-10-19 16:23:48 +0200

    removed dead code

--- src/main/java/me/schawe/autosnake/Autopilot.java
+++ src/main/java/me/schawe/autosnake/Autopilot.java
@@ -2,30 +2,20 @@ package me.schawe.autosnake;
 
 import org.deeplearning4j.nn.graph.ComputationGraph;
 import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
-import org.deeplearning4j.nn.modelimport.keras.exceptions.InvalidKerasConfigurationException;
-import org.deeplearning4j.nn.modelimport.keras.exceptions.UnsupportedKerasConfigurationException;
 import org.nd4j.common.io.ClassPathResource;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 
-import java.io.IOException;
 
 // https://deeplearning4j.konduit.ai/deeplearning4j/how-to-guides/keras-import
 public class Autopilot {
-    ComputationGraph modelFunctional;
-    String pathToModel;
+    ComputationGraph model;
 
     public Autopilot(String pathToModel) {
-        this.pathToModel = pathToModel;
-
         try {
             String savedModel = new ClassPathResource(pathToModel).getFile().getPath();
-            modelFunctional = KerasModelImport.importKerasModelAndWeights(savedModel, false);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (UnsupportedKerasConfigurationException e) {
-            e.printStackTrace();
-        } catch (InvalidKerasConfigurationException e) {
+            model = KerasModelImport.importKerasModelAndWeights(savedModel, false);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -38,7 +28,7 @@ public class Autopilot {
         INDArray input = Nd4j.create(state).reshape(1, state.length);
 
         // the output is also batched, since we have only one, we take the first with [0]
-        INDArray output = modelFunctional.output(input)[0];
+        INDArray output = model.output(input)[0];
 
         int action = output.ravel().argMax().getInt(0);
 
